b738ac2f89ea09c852481b8bedb2648160e0a4b7
dare-bea <twopenaboys@gmail.com>
2026-05-06 10:59:02 -0400

    simple canvas interaction

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = "silver";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let down = false;
+
+canvas.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    if (e.key === 'Enter') {
+        down = true;
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+canvas.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    if (e.key === 'Enter') {
+        down = false;
+        setTimeout(() => {
+            if (!down) {
+                ctx.fillStyle = "silver";
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+            }
+        }, 50)
+    }
+});
+
+canvas.focus();
